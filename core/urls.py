a19784109a8667277cf9308a00e01542ab0fa7f4
rowlindsay <rowlindsay@gmail.com>
2018-12-14 23:05:05 -0800

    basic users

--- core/urls.py
+++ core/urls.py
@@ -6,16 +6,23 @@ app_name = 'core'
 
 urlpatterns = [
 
+	# CORE PATH
 	# ex: /core/
 	path('', views.index_redirect, name="index_default"),	# needless redirect
 
+	# PROJECTS
 	# ex: /core/projects
 	path('projects', views.index, name="index"),	# projects list
-
 	# ex: /core/projects/22
 	path('projects/<int:project_id>/', views.project_detail, name='project_details'),
-
 	path('projects/submit/', views.project_create, name="project_create"),	# recieves POST info
-
 	path('projects/submit/form', views.project_form, name='project_submission_form'),
+
+	# USERS
+	path('login', views.login_form, name="user_login_form"),
+	path('login/redirect', views.user_login, name="user_login"),
+	path('logout', views.user_logout, name="user_logout"),
+	path('users/new/redirect', views.user_create, name='user_create'),
+	path('users/new', views.user_form, name='user_creation_form'),
+
 ]
--- core/views.py
+++ core/views.py
@@ -7,9 +7,17 @@ from .models import Project
 def index(request):
 	project_list = Project.objects.order_by('-init_date')[:10]	# top 10
 	template = loader.get_template('core/index.html')
+
 	context = {
 		'project_list': project_list,
+		'authed': False,
+		'user': '',
 	}
+
+	if request.user.is_authenticated:
+		context['authed'] = True,
+		context['user'] = request.user.username
+
 	return HttpResponse(template.render(context, request))
 
 def index_redirect(request):
@@ -33,3 +41,39 @@ def project_create(request):
 	p = Project(descriptor=descriptor, project_tag=tag, init_date=current_date)
 	p.save()
 	return HttpResponseRedirect(reverse('core:project_details', args=(p.id,)))
+
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+
+def login_form(request):
+	template = loader.get_template('core/login.html')
+	context = {}
+	return HttpResponse(template.render(context, request))
+
+def user_login(request):
+	uname = request.POST['username']
+	pwd = request.POST['password']
+	user = authenticate(request, username=uname, password=pwd)
+	if user is not None:
+		login(request, user)
+		return HttpResponseRedirect(reverse('core:index', args=()))
+	else:
+		return HttpResponse("invalid login")
+
+def user_logout(request):
+	logout(request)
+	return HttpResponse("successfully logged out")
+
+def user_form(request):
+	template = loader.get_template('core/newuser.html')
+	context = {}
+	return HttpResponse(template.render(context, request))
+
+def user_create(request):
+	uname = request.POST['username']
+	pwd = request.POST['password']
+	email = request.POST['email']
+	user = User.objects.create_user(uname, email, pwd)
+	user.save()
+	return HttpResponse("user created")
+
